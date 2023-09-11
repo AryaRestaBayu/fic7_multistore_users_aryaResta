466d888f79d82eb5ddbc7eb99b5c8f8b659eaee4
Arya Resta Bayu <aryaresta123@gmail.com>
2023-09-11 15:43:33 +0700

    first commit

---
+++ lib/common/global_variables.dart
@@ -0,0 +1,3 @@
+class GlobalVariables {
+  static const baseUrl = 'http://192.168.42.5:8000';
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:fic7_multistore/bloc/checkout/checkout_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fic7_multistore/bloc/categories/categories_bloc.dart';
+import 'package:fic7_multistore/bloc/logout/logout_bloc.dart';
+import 'package:fic7_multistore/bloc/products/products_bloc.dart';
+import 'package:fic7_multistore/data/datasources/auth_local_datasource.dart';
+import 'package:fic7_multistore/pages/auth/auth_page.dart';
+import 'package:fic7_multistore/pages/dashboard/dashboard_page.dart';
+import 'package:fic7_multistore/pages/splash/splash_page.dart';
+import 'package:fic7_multistore/utils/light_themes.dart';
+
+import 'bloc/login/login_bloc.dart';
+import 'bloc/register/register_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => LogoutBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ProductsBloc(),
+        ),
+        BlocProvider(
+          create: (context) => CategoriesBloc(),
+        ),
+        BlocProvider(
+          create: (context) => CheckoutBloc(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: light,
+          home: FutureBuilder<bool>(
+            future: AuthLocalDatasource().isLogin(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else if (snapshot.hasData && snapshot.data!) {
+                return const DashboardPage();
+              } else {
+                return const AuthPage();
+              }
+            },
+          )),
+    );
+  }
+}
